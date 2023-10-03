f61037b7ef292786ec558a8c50721738c15a5766
Kethelyn Gabrielly <126816177+kethelynsantos@users.noreply.github.com>
2023-10-03 11:28:49 -0300

    Add files via upload

--- src/Screens/Home.java
+++ src/Screens/Home.java
@@ -1,20 +1,63 @@
 package Screens;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Home extends JFrame{
     public Home() {
-        super("Image Display");
+        super("Rapidin");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(0, 0, 390, 844);
 
-        ImageIcon icon = new ImageIcon("src/Images/home.png");
-        JLabel label = new JLabel(icon);
-        add(label);
+        // Cria um painel principal para conter a imagem de fundo
+        JPanel mainPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                ImageIcon icon = new ImageIcon("src/Images/home.png");
+                Image image = icon.getImage();
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+
+        // Define um layout nulo para posicionar manualmente os componentes
+        mainPanel.setLayout(null);
+
+        JButton btn_rest = new JButton();
+        btn_rest.setBounds(22, 549, 96, 95);
+        btn_rest.setOpaque(false);
+        btn_rest.setContentAreaFilled(false);
+        btn_rest.setBorderPainted(false);
+
+        btn_rest.addActionListener(e -> {
+            Home home = new Home();
+            home.setVisible(true);
+            dispose(); // fecha a tela Home
+        });
+
+        JButton btn_user = new JButton();
+        btn_user.setBounds(22 + 96 + 27, 549, 96, 95);
+//        btn_user.setOpaque(false);
+//        btn_user.setContentAreaFilled(false);
+//        btn_user.setBorderPainted(false);
+
+        JButton btn_order = new JButton();
+        btn_order.setBounds(22 + 96 + 27 + 96 + 27, 549, 96, 95);
+//        btn_order.setOpaque(false);
+//        btn_order.setContentAreaFilled(false);
+//        btn_order.setBorderPainted(false);
+
+        // Adicione todos os botões ao painel principal
+        mainPanel.add(btn_rest);
+        mainPanel.add(btn_user);
+        mainPanel.add(btn_order);
+
+        // Define o painel principal como conteúdo da janela
+        setContentPane(mainPanel);
+
         setVisible(true);
         setLocationRelativeTo(null);
-
-
+        setResizable(false);
     }
 
     public static void main(String[] args) {
--- src/Screens/Start.java
+++ src/Screens/Start.java
@@ -9,19 +9,44 @@ public class Start extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(0, 0, 390, 844);
 
-        ImageIcon icon = new ImageIcon("src/Images/start.png");
-        JLabel label = new JLabel(icon);
-        Button btn_start = new Button("btn_start");
-        btn_start.setLocation(93, 719);
-        btn_start.addActionListener(e -> );
-        add(label);
+        // Cria um painel principal para conter a imagem de fundo
+        JPanel mainPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                ImageIcon icon = new ImageIcon("src/Images/start.png");
+                Image image = icon.getImage();
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+
+        // Define um layout nulo para posicionar manualmente os componentes
+        mainPanel.setLayout(null);
+
+        JButton btn_start = new JButton();
+        btn_start.setBounds(90, 685, 193, 48);
+        btn_start.setOpaque(false);
+        btn_start.setContentAreaFilled(false);
+        btn_start.setBorderPainted(false);
+
+        btn_start.addActionListener(e -> {
+            Home home = new Home();
+            home.setVisible(true);
+            dispose(); // fecha a tela Start
+        });
+
+        // Adiciona o botão ao painel principal
+        mainPanel.add(btn_start);
+
+        // Define o painel principal como conteúdo da janela
+        setContentPane(mainPanel);
+
         setVisible(true);
         setLocationRelativeTo(null);
-
+        setResizable(false);
     }
 
     public static void main(String[] args) {
         new Start();
     }
-
 }
